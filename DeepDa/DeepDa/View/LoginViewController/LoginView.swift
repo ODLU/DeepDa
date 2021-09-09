6561c58c5c043ad0d3868f97a1446c0d0cbd3988
KimWanki <seoulkwk95@gmail.com>
2021-09-10 00:59:33 +0900

    feat: LoginViewController init

---
+++ DeepDa/DeepDa/View/LoginViewController/LoginView.swift
@@ -0,0 +1,17 @@
+//
+//  LoginView.swift
+//  DeepDa
+//
+//  Created by WANKI KIM on 2021/09/10.
+//
+
+import UIKit
+
+class LoginView: BaseView {
+    override func setup() {
+        print("HI")
+    }
+    override func setupUI() {
+        print("Hello")
+    }
+}
---
+++ DeepDa/DeepDa/View/LoginViewController/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  DeepDa
+//
+//  Created by WANKI KIM on 2021/09/10.
+//
+
+import UIKit
+
+class LoginViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    override func setup() {
+        print("LoginView")
+    }
+}
